e8cd97dd0585b7b2255c28a2c58c5085daf05012
madhavam12 <66830856+madhavam12@users.noreply.github.com>
2021-08-21 15:55:01 +0530

    added theeeeeeeeee sha fingerrrprints and added the updated google servise file to prooject

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -29,27 +29,28 @@ class _LoginScreenState extends State<LoginScreen> {
               Container(
                 margin: EdgeInsets.fromLTRB(30.0, 5.0, 30.0, 5.0),
                 child: new RaisedButton(
-                    padding: EdgeInsets.only(
-                        top: 3.0, bottom: 3.0, left: 15.0, right: 15),
-                    color: const Color(0xFF4285F4),
-                    onPressed: () async {},
-                    child: new Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        new Image.asset(
-                          'assets/images/google_logo.png',
-                          height: 48.0,
+                  padding: EdgeInsets.only(
+                      top: 3.0, bottom: 3.0, left: 15.0, right: 15),
+                  color: const Color(0xFF4285F4),
+                  onPressed: () async {},
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/images/google_logo.png',
+                        height: 48.0,
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                        child: new Text(
+                          "Sign in with Google",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
                         ),
-                        new Container(
-                            padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                            child: new Text(
-                              "Sign in with Google",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
-                            )),
-                      ],
-                    )),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
