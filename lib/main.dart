aef44b4013cef96db3a8f9c0de7094a58851edb6
madhavam12 <66830856+madhavam12@users.noreply.github.com>
2021-08-21 16:41:45 +0530

    Added the firebase auth service nd linked it with UI and created homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
 
 import 'screens/loginScreen.dart';
+import 'package:simple_connection_checker/simple_connection_checker.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:async/async.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:async';
 
-void main() {
+void main() async {
+  SimpleConnectionChecker _simpleConnectionChecker = SimpleConnectionChecker()
+    ..setLookUpAddress('google.com');
+
+  _simpleConnectionChecker.onConnectionChange.listen((connected) {
+    if (!connected) {
+      Fluttertoast.showToast(
+          msg: "No internet connection, please connect to the internet",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.CENTER,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    }
+  });
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(home: LoginScreen()));
 }
