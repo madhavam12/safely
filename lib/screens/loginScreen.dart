5c31c988460ae92ff5fdcd50f7caf975abe84d15
madhavam12 <66830856+madhavam12@users.noreply.github.com>
2021-08-21 15:21:04 +0530

    updated the pubspec file and created the ui of ligin page

---
+++ lib/screens/loginScreen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Image.asset("assets/images/login.png", height: 250),
+              Text(
+                "Tab below to login",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: "QuickSand",
+                  fontSize: 25,
+                ),
+              ),
+              SizedBox(height: 30),
+              Container(
+                margin: EdgeInsets.fromLTRB(30.0, 5.0, 30.0, 5.0),
+                child: new RaisedButton(
+                    padding: EdgeInsets.only(
+                        top: 3.0, bottom: 3.0, left: 15.0, right: 15),
+                    color: const Color(0xFF4285F4),
+                    onPressed: () async {},
+                    child: new Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        new Image.asset(
+                          'assets/images/google_logo.png',
+                          height: 48.0,
+                        ),
+                        new Container(
+                            padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                            child: new Text(
+                              "Sign in with Google",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                      ],
+                    )),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
