991e9722049b81c95531f42cf7544d6f0805f859
madhavam12 <66830856+madhavam12@users.noreply.github.com>
2021-08-22 10:05:59 +0530

    wrote the cloud function, deploying and testing now

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,43 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().functions);
+
+var newData;
+
+exports.helpRequest = functions.firestore.document('requests/{id}').onCreate(async(snapshot, context) => {
+
+
+    if (snapshot.empty) {
+        console.log('No Devices');
+        return;
+    }
+
+
+    //device tokens
+    const tokens = snapshot.data().deviceTokensNearby;
+
+    var payload = {
+        //payload below
+        notification: {
+            priority: "high",
+
+            title: 'Someone needs help!',
+            body: 'Tap here to know more.',
+
+        },
+        data: {
+            "click_action": "FLUTTER_NOTIFICATION_CLICK",
+
+        },
+    };
+
+    try {
+        const response = await admin.messaging().sendToDevice(tokens, payload);
+        console.log('Notification sent successfully');
+
+
+    } catch (err) {
+        console.log(err);
+    }
+});
